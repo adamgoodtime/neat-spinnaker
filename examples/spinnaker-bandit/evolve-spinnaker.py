Multi-armed bandit prints rewards in correct places now, brought NEAT into the future, delays are rounded to stop synaptic overflow bug, enable_advanced_monitor_support=True

examples/spinnaker-bandit/evolve-spinnaker.py
@@ -11,16 +11,19 @@ import csv
 import numpy as np
 import time
 
-exec_thing = 'logic'
+exec_thing = 'arms'
 shared_probabilities = True
 shape_fitness = True
-spike_fitness = False # 'out'
-reward_based = 0
+spike_fitness = False
 spike_weight = 0.1
 noise_rate = 0
 noise_weight = 0.01
 fast_membrane = False
 parse_conn = False
+plasticity = True
+delay = 25
+
+threading_tests = True
 
 try_attempts = 2
 all_fails = 0
@@ -31,25 +34,43 @@ empty_post_count = 0
 
 '''remember to change inputs and outputs in the config as well'''
 
-#arm params
-arms_runtime = 41000
-duration_of_trial = 200
+#arms params
+arms_runtime = 20000
+constant_input = 1
+arms_stochastic = 1
+arms_rate_on = 20
+arms_rate_off = 5
+random_arms = 0
 arm1 = 1
 arm2 = 0
-# arm3 = 0.1
+arm3 = 0.1
 arm_len = 1
 arms = []
+arms_reward = 1
 for i in range(arm_len):
     arms.append([arm1, arm2])
     arms.append([arm2, arm1])
-# arms = [[0, 1], [1, 0]]
-# arms = [[0, 1]]
-arms = [[0.4, 0.6], [0.6, 0.4], [0.3, 0.7], [0.7, 0.3], [0.2, 0.8], [0.8, 0.2], [0.1, 0.9], [0.9, 0.1], [0, 1], [1, 0]]
+    # for arm in list(itertools.permutations([arm1, arm2, arm3])):
+    #     arms.append(list(arm))
+# arms = [[0.4, 0.6], [0.6, 0.4], [0.3, 0.7], [0.7, 0.3], [0.2, 0.8], [0.8, 0.2], [0.1, 0.9], [0.9, 0.1]]
+# arms = [[0.4, 0.6], [0.6, 0.4], [0.3, 0.7], [0.7, 0.3], [0.2, 0.8], [0.8, 0.2], [0.1, 0.9], [0.9, 0.1], [0, 1], [1, 0]]
+'''top_prob = 1
+low_prob = 0
+med_prob = 0.1
+hii_prob = 0.2
+arms = [[low_prob, med_prob, top_prob, hii_prob, med_prob, low_prob, med_prob, low_prob], [top_prob, low_prob, low_prob, med_prob, hii_prob, med_prob, low_prob, med_prob],
+        [hii_prob, top_prob, med_prob, low_prob, low_prob, med_prob, med_prob, low_prob], [med_prob, low_prob, low_prob, top_prob, med_prob, hii_prob, low_prob, med_prob],
+        [low_prob, low_prob, low_prob, med_prob, top_prob, med_prob, hii_prob, med_prob], [low_prob, med_prob, low_prob, med_prob, med_prob, top_prob, low_prob, hii_prob],
+        [med_prob, low_prob, hii_prob, low_prob, med_prob, low_prob, top_prob, med_prob], [low_prob, hii_prob, med_prob, med_prob, low_prob, med_prob, low_prob, top_prob]]
+# '''
 
 #pendulum params
 pendulum_runtime = 181000
+double_pen_runtime = 60000
+pendulum_delays = 1
+max_fail_score = 0
 no_v = False
-encoding = 1
+encoding = 0
 time_increment = 20
 pole_length = 1
 pole2_length = 0.1
@@ -64,9 +85,12 @@ tau_force = 0
 
 #logic params
 logic_runtime = 5000
-stochastic = 1
 score_delay = 5000
+logic_stochastic = 1
+logic_rate_on = 20
+logic_rate_off = 5
 truth_table = [0, 1, 1, 0]
+# truth_table = [0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0]
 input_sequence = []
 segment = [0 for j in range(int(np.log2(len(truth_table))))]
 input_sequence.append(segment)
@@ -79,76 +103,173 @@ for i in range(1, len(truth_table)):
         current_value -= 2**highest_power
     input_sequence.append(segment)
 
-weight_max = 1.0
-weight_scale = 1.0
-delay = 1
+#Recall params
+recall_runtime = 60000
+recall_rate_on = 50
+recall_rate_off = 0
+recall_pop_size = 1
+prob_command = 1./6.
+prob_in_change = 1./2.
+time_period = 200
+recall_stochastic = 1
+recall_reward = 0
+recall_parallel_runs = 2
 
-weight = 0.1
+#MNIST
+max_freq = 5000
+on_duration = 1000
+off_duration = 1000
+data_size = 200
+mnist_parallel_runs = 2
+mnist_runtime = data_size * (on_duration + off_duration)
 
-threading_tests = True
+#erbp params
+erbp_runtime = 20
+erbp_max_depth = [5, 100]
 
-iteration_count = 0
+#breakout params
+breakout_runtime = 181000
+x_factor = 8
+y_factor = 8
+bricking = 0
 
-if exec_thing == 'pen':
+inputs = 0
+outputs = 0
+test_data_set = []
+config = ''
+runtime = 0
+stochastic = -1
+rate_on = 0
+rate_off = 0
+
+if exec_thing == 'br':
+    runtime = breakout_runtime
+    inputs = (160 / x_factor) * (128 / y_factor)
+    outputs = 2
+    config = 'bout {}-{}-{} '.format(x_factor, y_factor, bricking)
+    test_data_set = 'something'
+    number_of_tests = 'something'
+elif exec_thing == 'pen':
     runtime = pendulum_runtime
+    constant_delays = pendulum_delays
     encoding = 1
-    input_size = number_of_bins * 4
+    inputs = 4
+    if encoding != 0:
+        inputs *= number_of_bins
     if no_v:
-        input_size /= 2
-    output_size = 2
+        inputs /= 2
+    outputs = 2
     config = 'pend-an{}-{}-F{}-R{}-B{}-O{} '.format(pole_angle[0], len(pole_angle), force_increments, max_firing_rate, number_of_bins, bin_overlap)
     test_data_set = pole_angle
+    number_of_tests = len(pole_angle)
 elif exec_thing == 'rank pen':
     runtime = pendulum_runtime
-    input_size = number_of_bins * 4
+    constant_delays = pendulum_delays
+    inputs = 4 * number_of_bins
     if no_v:
-        input_size /= 2
-    output_size = force_increments
-    config = 'rank pend-an{}-{}-F{}-R{}-B{}-O{}-E{} '.format(pole_angle[0], len(pole_angle), force_increments, max_firing_rate, number_of_bins, bin_overlap, encoding)
+        config += "\b-no_v "
+        inputs /= 2
+    outputs = force_increments
+    config = 'rank-pend-an{}-{}-F{}-R{}-B{}-O{}-E{} '.format(pole_angle[0], len(pole_angle), force_increments, max_firing_rate, number_of_bins, bin_overlap, encoding)
     test_data_set = pole_angle
+    number_of_tests = len(pole_angle)
 elif exec_thing == 'double pen':
-    runtime = pendulum_runtime
-    input_size = number_of_bins * 6
+    runtime = double_pen_runtime
+    constant_delays = pendulum_delays
+    inputs = 6 * number_of_bins
     if no_v:
-        input_size /= 2
-    output_size = 2
-    config = 'double pend-an{}-{}-F{}-R{}-B{}-O{} '.format(pole_angle[0], len(pole_angle), force_increments, max_firing_rate, number_of_bins, bin_overlap)
+        inputs /= 2
+    outputs = force_increments
+    config = 'double-pend-an{}-{}-pl{}-{}-F{}-R{}-B{}-O{} '.format(pole_angle[0], len(pole_angle), pole_length, pole2_length, force_increments, max_firing_rate, number_of_bins, bin_overlap)
     test_data_set = pole_angle
+    number_of_tests = len(pole_angle)
+elif exec_thing == 'arms':
+    stochastic = arms_stochastic
+    rate_on = arms_rate_on
+    rate_off = arms_rate_off
+    if isinstance(arms[0], list):
+        number_of_arms = len(arms[0])
+    else:
+        number_of_arms = len(arms)
+    runtime = arms_runtime
+    test_data_set = arms
+    inputs = 2
+    outputs = number_of_arms
+    duration_of_trial = 200
+    if random_arms:
+        config = 'bandit-rand-{}-{} '.format(arms[0][0], len(arms))
+    else:
+        config = 'bandit-{}-{} '.format(arms[0][0], len(arms))
+    if constant_input:
+        if stochastic:
+            config += 'stoc '
+        config += 'on-{} off-{} r{} '.format(rate_on, rate_off, arms_reward)
+    number_of_tests = len(arms)
 elif exec_thing == 'logic':
+    stochastic = logic_stochastic
     runtime = logic_runtime
+    rate_on = logic_rate_on
+    rate_off = logic_rate_off
     test_data_set = input_sequence
-    input_size = len(input_sequence[0])
-    output_size = 2
+    number_of_tests = len(input_sequence)
+    inputs = len(input_sequence[0])
+    outputs = 2
     if stochastic:
         config = 'logic-stoc-{}-run{}-sample{} '.format(truth_table, runtime, score_delay)
     else:
         config = 'logic-{}-run{}-sample{} '.format(truth_table, runtime, score_delay)
-elif exec_thing == 'arms':
-    runtime = arms_runtime
-    input_size = 2
-    output_size = len(arms[0])
-    config = 'bandit {}-{}'.format(arms[0], len(arms))
-    test_data_set = arms
+    config += 'on-{} off-{} '.format(rate_on, rate_off)
+elif exec_thing == 'recall':
+    stochastic = recall_stochastic
+    runtime = recall_runtime
+    rate_on = recall_rate_on
+    rate_off = recall_rate_off
+    for j in range(recall_parallel_runs):
+        test_data_set.append([j])
+    number_of_tests = recall_parallel_runs
+    inputs = 4 * recall_pop_size
+    outputs = 2
+    if stochastic:
+        config = 'recall-stoc-pop_s{}-run{}-in_p{}-r_on{} '.format(recall_pop_size, runtime, prob_in_change, rate_on)
+    else:
+        config = 'recall-pop_s{}-run{}-in_p{}-r_on{} '.format(recall_pop_size, runtime, prob_in_change, rate_on)
+elif exec_thing == 'mnist':
+    runtime = mnist_runtime
+    for j in range(mnist_parallel_runs):
+        test_data_set.append([j])
+    number_of_tests = mnist_parallel_runs
+    inputs = 28*28
+    outputs = 10
+    config = 'mnist-freq-{}-on-{}-off-{}-size-{} '.format(max_freq, on_duration, off_duration, data_size)
+elif exec_thing == 'erbp':
+    maximum_depth = erbp_max_depth
+    make_action = False
+    runtime = erbp_runtime
+    inputs = 0
+    outputs = 0
+    test_data_set = [[0], [1]]
+    number_of_tests = len(test_data_set)
+    config = 'erbp {} {} '.format(runtime, maximum_depth)
 else:
-    print("Incorrect setup chosen, please try agian")
+    print("\nNot a correct test setting\n")
     raise Exception
+if plasticity:
+    config += 'pl '
 
-config += ' reward {}'.format(reward_based)
 if spike_fitness:
     if spike_fitness == 'out':
-        config += ' out-spikes'
+        config += 'out-spikes '
     else:
-        config += ' spikes'
+        config += 'spikes '
 if shape_fitness:
-    config += ' shape'
+    config += 'shape '
 if noise_rate:
-    config += ' noise {}-{}'.format(noise_rate, noise_weight)
+    config += 'noise {}-{} '.format(noise_rate, noise_weight)
 if fast_membrane:
-    config += ' fast_mem'
+    config += 'fast_mem '
 if parse_conn:
-    config += ' parse_conn'
-if no_v:
-    config += ' no_v'
+    config += 'parse_conn '
+config += 'delay-{}'.format(delay)
 
 
 best_fitness = []
@@ -159,6 +280,10 @@ average_score = []
 worst_score = []
 
 stats = None
+
+weight = 0.1
+
+iteration_count = 0
 
 def read_fitnesses(config, max_fail_score=0):
     # fitnesses = []
@@ -303,11 +428,12 @@ def spinn_genomes(genomes, neat_config):
     iteration_count += 1
     best_score.append(best_total)
     print("best scores: ", best_score)
-    average_score.append(np.average(combined_fitnesses))
+    average_score.append(np.average(combined_scores))
     print("average scores: ", average_score)
-    worst_score.append(min(combined_fitnesses))
+    worst_score.append(min(combined_scores))
     print("worst scores: ", worst_score)
     print("\n", end=" ")
+    save_stats()
     i = 0
     print(config)
     for genome_id, genome in genomes: